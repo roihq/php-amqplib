4ecf603e502fd71d0b8a1054726047a11f9e4504
Dennis Havelock <dennis.havelock@gettyimages.com>
2014-12-22 10:38:37 -0700

    Test 8

--- tests/config.php
+++ tests/config.php
@@ -1,8 +1,8 @@
 <?php
 
-define('HOST', 'localhost');
+define('HOST', 'dev-dhavelock.istockphoto.com');
 define('PORT', 5672);
 define('USER', 'phpamqplib');
 define('PASS', 'phpamqplib_password');
 define('VHOST', 'phpamqplib_testbed');
-define('AMQP_DEBUG', false);
+define('AMQP_DEBUG', true);
