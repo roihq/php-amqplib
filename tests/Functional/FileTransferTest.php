bf242387421fd488f36fd6001a936b13c57b2d06
Dennis Havelock <dennis.havelock@gettyimages.com>
2014-12-23 16:31:25 -0700

    Test 33

--- tests/Functional/FileTransferTest.php
+++ tests/Functional/FileTransferTest.php
@@ -29,30 +29,23 @@ class FileTransferTest extends \PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        parent::setUp();
-    }
-
-
-
-    public function testSendFile()
-    {
         $this->conn = new AMQPConnection(HOST, PORT, USER, PASS, VHOST);
         $this->ch = $this->conn->channel();
 
         $this->ch->exchange_declare($this->exchange_name, 'direct', false, false, false);
         list($this->queue_name, ,) = $this->ch->queue_declare();
         $this->ch->queue_bind($this->queue_name, $this->exchange_name, $this->queue_name);
-        $this->flush();
+    }
 
+
+
+    public function testSendFile()
+    {
         $this->msg_body = file_get_contents(__DIR__ . '/fixtures/data_1mb.bin');
-        $this->flush();
-        //$this->msg_body = 'yes';
 
         $msg = new AMQPMessage($this->msg_body, array('delivery_mode' => 1));
-        $this->flush();
-        $this->ch->basic_publish($msg, $this->exchange_name, $this->queue_name);
-        $this->flush();
 
+        $this->ch->basic_publish($msg, $this->exchange_name, $this->queue_name);
 
         $this->ch->basic_consume(
             $this->queue_name,
@@ -63,22 +56,9 @@ class FileTransferTest extends \PHPUnit_Framework_TestCase
             false,
             array($this, 'process_msg')
         );
-        $this->flush();
+
         while (count($this->ch->callbacks)) {
             $this->ch->wait();
-            $this->flush();
-        }
-        $this->flush();
-
-        if ($this->ch) {
-            $this->ch->exchange_delete($this->exchange_name);
-            $this->ch->close();
-            $this->flush();
-        }
-
-        if ($this->conn) {
-            $this->conn->close();
-            $this->flush();
         }
     }
 
@@ -86,33 +66,25 @@ class FileTransferTest extends \PHPUnit_Framework_TestCase
 
     public function process_msg($msg)
     {
-        $this->flush();
-
-        echo '+++++++++++++++++++++++';
-        //var_dump($msg);
         $delivery_info = $msg->delivery_info;
 
         $delivery_info['channel']->basic_ack($delivery_info['delivery_tag']);
-        $this->flush();
         $delivery_info['channel']->basic_cancel($delivery_info['consumer_tag']);
-        $this->flush();
 
         $this->assertEquals($this->msg_body, $msg->body);
-        $this->flush();
-        
-    }
-
-    public function flush()
-    {
-        //@ob_flush();
-        //flush();
     }
 
 
-    /*
+
     public function tearDown()
     {
-        
+        if ($this->ch) {
+            $this->ch->exchange_delete($this->exchange_name);
+            $this->ch->close();
+        }
+
+        if ($this->conn) {
+            $this->conn->close();
+        }
     }
-    */
 }
